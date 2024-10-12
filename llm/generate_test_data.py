a5541b09c5a3f44814faee8ca2581f22532e33a6
Iker Fuentes <A01749675@tec.mx>
2024-10-12 17:26:18 -0600

    upda

--- llm/generate_test_data.py
+++ llm/generate_test_data.py
@@ -5,4 +5,16 @@ categories = ['prompt', 'category', 'information_context','user_context']
 
 def generate_dummy():
     ai = AiRequests(Objective.DUMMY, "You must generate dummy data for testing")
-    print(ai.make_prompt("give me examples"))
+    print(ai.make_prompt('''Make json data for testing folowing the format: 
+                         
+                                 {
+            "prompt": "user input",
+            "category": "category",
+            "information_context" :"data to reference"
+            "user_context" : "Information about the user"
+        }
+                         
+                         ''')) 
+    
+if __name__ == "__main__":
+    generate_dummy()
--- tes_firebase.py
+++ tes_firebase.py
@@ -5,5 +5,7 @@ db = firestore.Client(database='hack-banorte')
 doc_ref = db.collection(u'user_data').document(u'B0nDZBfLZrqkcT1eycX4')
 
 
+
+print(db.collection(u'user_data').stream())
 for doc in db.collection(u'user_data').stream():
     print(f'{doc.id} => {doc.to_dict()}')
