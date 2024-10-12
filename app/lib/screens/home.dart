3fc568e7c20d07965bc3f138265ac83238cd6bfa
A01798671 <a01798671@tec.mx>
2024-10-12 17:44:01 -0600

    custom_navBar

--- app/lib/screens/home.dart
+++ app/lib/screens/home.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
+import '../widgets/custom_nav.dart'; // Asegúrate de importar el archivo donde creaste el CustomNavBar
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+      // Aquí puedes agregar lógica para navegar o cambiar de contenido dependiendo del índice seleccionado
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Home'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Banorte AI'),
+      ),
+      body: Center(
+        child: Text('Selected Tab: $_selectedIndex'),
+      ),
+      bottomNavigationBar: CustomNavBar(
+        onTabSelected: _onItemTapped,
+        selectedIndex: _selectedIndex,
+      ),
     );
   }
 }
