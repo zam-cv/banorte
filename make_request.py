e5c71b0dcfd9f815892977cc46ff9f52f4fcef38
Iker Fuentes <A01749675@tec.mx>
2024-10-12 14:16:39 -0600

    Class update

--- make_request.py
+++ make_request.py
@@ -1,5 +1,6 @@
 import vertexai
 import enum
+import json
 from CONSTS import *
 
 from vertexai.generative_models import (
@@ -10,13 +11,9 @@ from vertexai.generative_models import (
 )
 
 class Objective(enum.Enum):
-    TRANSLATE = "You are a helpful translator."
-    SUMMARIZE = "You are a helpful text summarizer."
-    SENTIMENT_ANALYSIS =  "You are a helpful sentiment analyzer."
-    QUESTION_ANSWERING = "You are a helpful question answerer."
-    CODE_GENERATION = "You are a helpful code generator."
-    TEXT_GENERATION = "You are a helpful text generator."
-    FINANCIAL_TEACHER = "YOU ARE A FINANCE TEACHER"
+    
+    BANORTE_ASSISTANT = "You are a helpful Banorte assistant. "
+    
 
 class AiRequests():
     
@@ -57,8 +54,45 @@ class AiRequests():
             safety_settings=self.safety_settings,
             )
         return response.text
+    
+    def make_prompt_with_context(self,prompt:str,context:str)->str:
+        prompt :str = f"""
+                User input: {prompt}.
+                Answer:
+                """
+        self.contents.append(context)
+        self.contents.append(prompt)
+        response = self.model.generate_content(
+            self.contents,
+            generation_config=self.generation_config,
+            safety_settings=self.safety_settings,
+            )
+        return response.text 
+    
+    def make_prompt_with_from_json(self,prompt:str, context:json)->str:
+        prompt :str = f"""
+                User input: {prompt}.
+                Answer:
+                """
+        data = json.loads(context)
+        category = data["category"]
+        document = data["document"]
+        history : list[str] = data["history"]
+        
+        self.contents.append(category)
+        self.contents.append(document)
+        self.contents.append(history)
+        
+        self.contents.append(prompt)
+        response = self.model.generate_content(
+            self.contents,
+            generation_config=self.generation_config,
+            safety_settings=self.safety_settings,
+            )
+        return response.text 
 
 
 if __name__ == "__main__":
-    ai = AiRequests(Objective.TRANSLATE,"You must translate to spansih")
-    print(ai.make_prompt("I like bagels."))
+    ai = AiRequests(Objective.BANORTE_ASSISTANT,"You must translate to spansih")
+    #print(ai.make_prompt("I like bagels."))
+    print(ai.make_prompt_with_context("What is financial health","Financial health is about the overall state of your personal finances. It includes having a balanced budget, a manageable level of debt, a good credit score, and an emergency fund. It also involves planning for the future through savings and investments, ensuring you can meet both your short-term and long-term financial goals. Maintaining good financial health means regularly reviewing and managing your finances to keep everything in check. It's like a fitness plan but for your money!"))
