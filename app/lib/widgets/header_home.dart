76eb8b42f2d23e2990638b4f2706e45d97ae7d86
eduardohufg <eduardochavezmartin10@gmail.com>
2024-10-12 21:25:41 -0600

    Add: Header

--- app/lib/widgets/header_home.dart
+++ app/lib/widgets/header_home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:app/screens/profile.dart'; // Asegúrate de tener importada la pantalla Profile
 
 class HeaderHome extends StatelessWidget {
   final int number;
@@ -12,11 +13,11 @@ class HeaderHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Cálculo para el numerador y denominador
-    int denominator = (number <= 7) ? 7 : ((number ~/ 7) * 7);
+    int denominator = (number <= 7) ? 7 : ((number / 7).ceil() * 7);
 
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
-      color: const Color(0xFF0A0E21), // Color de fondo oscuro como en la imagen
+      color: const Color(0xFF070D2A), // Color de fondo oscuro como en la imagen
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
@@ -44,11 +45,21 @@ class HeaderHome extends StatelessWidget {
             'assets/logo.svg', // Ruta del logo.svg
             height: 24, // Ajusta el tamaño del logo
           ),
-          // Icono de usuario (lado derecho)
-          SvgPicture.asset(
-            'assets/user.svg', // Ruta del icono user.svg
-            height: 24, // Ajusta el tamaño del icono
-            color: Colors.red, // Color rojo para el icono
+          // Icono de usuario (lado derecho) envuelto en GestureDetector
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        const ProfilePage()), // Navegamos a Profile
+              );
+            },
+            child: SvgPicture.asset(
+              'assets/user.svg', // Ruta del icono user.svg
+              height: 24, // Ajusta el tamaño del icono
+              color: Colors.red, // Color rojo para el icono
+            ),
           ),
         ],
       ),
