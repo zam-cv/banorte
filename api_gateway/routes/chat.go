03f3183049dace6a9bcbaa4bce115c9b845b143e
Carlos Zamudio <a01799283@tec.mx>
2024-10-13 04:10:00 -0600

    update

--- api_gateway/routes/chat.go
+++ api_gateway/routes/chat.go
@@ -2,6 +2,10 @@ package routes
 
 import (
 	"api_gateway/middlewares"
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,6 +16,9 @@ type ChatMessage struct {
 }
 
 type ChatResponse struct {
+	Question      string   `json:"question" binding:"required"`
+	Options       []string `json:"options" binding:"required"`
+	CorrectAnswer string   `json:"correct_answer" binding:"required"`
 }
 
 func addChatRoutes(rg *gin.RouterGroup) {
@@ -24,9 +31,32 @@ func addChatRoutes(rg *gin.RouterGroup) {
 			return
 		}
 
-		ctx.JSON(200, gin.H{
-			"message": "Mensaje enviado",
-		})
+		jsonData, err := json.Marshal(message)
+		if err != nil {
+			ctx.JSON(500, gin.H{"error": "Error al preparar la solicitud"})
+			return
+		}
+
+		resp, err := http.Post("http://127.0.0.1:8000/model/selection/", "application/json", bytes.NewBuffer(jsonData))
+		if err != nil {
+			ctx.JSON(500, gin.H{"error": "Error al realizar la solicitud al servicio externo"})
+			return
+		}
+		defer resp.Body.Close()
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			ctx.JSON(500, gin.H{"error": "Error al leer la respuesta del servicio externo"})
+			return
+		}
+
+		var chatResponse ChatResponse
+		if err := json.Unmarshal(body, &chatResponse); err != nil {
+			ctx.JSON(500, gin.H{"error": "Error al procesar la respuesta del servicio externo"})
+			return
+		}
+
+		ctx.JSON(200, chatResponse)
 	})
 
 	group.POST("/practice", middlewares.AuthMiddleware(), func(ctx *gin.Context) {
