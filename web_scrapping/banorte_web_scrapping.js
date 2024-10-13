64719945e847ebcc07e8ae3af826b9185b7cab40
Carlos Zamudio <a01799283@tec.mx>
2024-10-13 06:33:45 -0600

    update

--- web_scrapping/banorte_web_scrapping.js
+++ web_scrapping/banorte_web_scrapping.js
@@ -5,7 +5,7 @@ const cheerio = require('cheerio');
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
